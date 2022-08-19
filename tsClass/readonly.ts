c9937e93cf1a0f2cf3dd4ad7c2b94d360bbd6007
linth <zhua0404@gmail.com>
2022-08-19 11:34:41 +0800

    add readonly example.

---
+++ tsClass/readonly.ts
@@ -0,0 +1,22 @@
+/**
+ * readonly 
+ *  - Use the readonly access modifier to mark a class property as immutable.
+ *  - A readonly property must be initialized as a part of the declaration or in the constructor of the same class.
+ * 
+ * 
+ * Reference:
+ *  - https://www.typescripttutorial.net/typescript-tutorial/typescript-readonly/
+ */
+
+class Student {
+    // TODO: need to check.
+    readonly birthday: Date; // happen 'SyntaxError: Unexpected identifier'
+
+    constructor(birthday) {
+        this.birthday = birthday;
+    }
+}
+
+
+let s = new Student(new Date(2020, 1, 1));
+console.log(s.birthday);
