441bf1b0812bc97131b6410cd80e84104e799522
linth <zhua0404@gmail.com>
2022-08-25 15:43:52 +0800

    add modules example.

---
+++ tsModules/newModules.ts
@@ -0,0 +1,19 @@
+
+// import class by export.
+export class firstModule {
+
+    firstFunction(): void {
+        console.log(`this is first function.`);
+    }
+}
+
+export class SecondModule {
+
+    secondFunction(): void {
+        console.log(`this is second function.`);
+    }
+}
+
+
+// import type case by export.
+export type alphaNumeric = string;
--- tsModules/tsModulesBase.ts
+++ tsModules/tsModulesBase.ts
@@ -1,7 +1,16 @@
 /**
  * TypeScript modules
  *  - Since `ES6`, JavaScript started supporting modules as the native part of the language.
- *  - 
+ *  - `export`
+ *  - `as`: rename your modules
+ *  - import type
+ *  - Re-exports
+ * 
+ * 
+ *  - TypeScript shares the same module concept with ES6 module. A module can contain both declarations and code.
+ *  - In a module, variables, functions, classes, interfaces, etc., executes on its own scope, not the global scope.
+ *  - Use `export` statement to export variables, functions, classes, interfaces, type, etc., from a module.
+ *  - Use `import` statement to access exports from other modules.
  * 
  * 
  * Reference:
@@ -20,5 +29,28 @@ interface ValidatorAnother {
 }
 
 export { ValidatorAnother };
+export { ValidatorAnother as UserCustomDefined } // you can rename your modules.
+
+
+// importing a new module.
+import { firstModule } from './newModules';
+let fm = new firstModule();
+fm.firstFunction(); // this is first function.
+
+import { SecondModule as s2 } from "./newModules";
+let s = new s2();
+s.secondFunction(); // this is second function.
+
+
+// import types.
+import { alphaNumeric } from "./newModules";
+
+function isAlphType(a: alphaNumeric): boolean {
+    console.log(a, typeof(a));    
+    return false;
+}
+
+isAlphType('hello world'); // hello world string
+
 
 
