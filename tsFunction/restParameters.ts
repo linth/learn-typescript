3e4e9651902d997078502c2e52228945abf8cf44
linth <zhua0404@gmail.com>
2022-08-26 18:11:58 +0800

    add rest parameters example.

---
+++ tsFunction/restParameters.ts
@@ -0,0 +1,25 @@
+/**
+ * Rest Parameters
+ *  - use the rest parameters to handle an infinite number of arguments of a function.
+ * 
+ * 
+ * Reference:
+ *  - https://www.typescripttutorial.net/typescript-tutorial/typescript-rest-parameters/
+ */
+
+function getTotal(...number: number[]): number {
+    let total = 0;
+    number.forEach( (item) => total += item);
+    return total;
+}
+
+console.log(getTotal()); // 0
+console.log(getTotal(10, 20)); // 30
+console.log(getTotal(10, 20, 30)); // 60
+
+
+
+
+
+
+
