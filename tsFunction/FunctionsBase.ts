42aa2916df0aeaff56b3c3f5dbe7e76b5bc2cf41
linth <zhua0404@gmail.com>
2022-08-26 13:14:46 +0800

    add function base example.

---
+++ tsFunction/FunctionsBase.ts
@@ -0,0 +1,16 @@
+/**
+ * Function
+ * 
+ * 
+ * Reference:
+ *  - https://www.typescripttutorial.net/typescript-tutorial/typescript-functions/
+ */
+
+function addTwoNumbers(a: number, b: number): number {
+    return a+b;
+}
+
+let sum = add(10, 20);
+console.log(sum);
+
+
