47449d7fb6eb8785459ae8864fcb408647d8fd30
linth <zhua0404@gmail.com>
2022-08-28 20:42:47 +0800

    add do-while example.

---
+++ controlFlowStatements/doWhile.ts
@@ -0,0 +1,22 @@
+/**
+ * do…while statement
+ * 
+ * 
+ * Reference:
+ *  - https://www.typescripttutorial.net/typescript-tutorial/typescript-do-while/
+ */
+
+let doWhileParam = 0;
+
+do {
+    console.log(doWhileParam);
+    doWhileParam++;
+} while (doWhileParam<5);
+
+/** Results:
+0
+1
+2
+3
+4
+ */
