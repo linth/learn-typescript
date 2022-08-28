a8f3f19bab8389850cce4f98062df5bebe700b93
linth <zhua0404@gmail.com>
2022-08-28 20:38:14 +0800

    add while-loop example.

---
+++ controlFlowStatements/whileLoop.ts
@@ -0,0 +1,17 @@
+/**
+ * while-loop
+ *  - create a pretest loop that executes as long as a condition is true.
+ *  - Use the TypeScript while statement to create a loop that will run as long as a condition is true.
+ * 
+ * 
+ * Reference:
+ *  - https://www.typescripttutorial.net/typescript-tutorial/typescript-while/
+ */
+
+let whileParam = 0;
+
+while (whileParam < 5) {
+    console.log(whileParam);
+    whileParam++;
+}
+
