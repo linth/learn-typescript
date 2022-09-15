95772e0e3d9ff37d9f219d04128fb6beb4be5266
linth <zhua0404@gmail.com>
2022-09-15 23:14:18 +0800

    add Number type example.

---
+++ tsBasicTypes/NumberType.ts
@@ -0,0 +1,27 @@
+/**
+ * Number type
+ * 
+ * Reference:
+ *  - https://www.typescripttutorial.net/typescript-tutorial/typescript-number/
+ */
+
+let price: number;
+price = 9.95;
+console.log(price);
+
+
+// Decimal numbers
+let animalCounter: number = 0;
+let x: number = 100, 
+    y: number = 200;
+
+
+// Binary Numbers
+let bin = 0b100;
+let anotherBin: number = 0B010;
+
+
+// Big Integers
+// The big integers represent the whole numbers larger than 253 – 1. A Big integer literal has the n character at the end of an integer literal like this:
+let big: bigint = 9007199254740991n;
+
