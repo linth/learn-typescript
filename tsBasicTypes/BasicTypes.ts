d5a8251f13a0fc2bac2ac72888d8409ea8e07b75
linth <zhua0404@gmail.com>
2022-09-27 19:33:39 +0800

    add basic type example comments.

---
+++ tsBasicTypes/BasicTypes.ts
@@ -0,0 +1,23 @@
+/**
+ * 基礎類型 basic type.
+ *  - boolean
+ *  - number
+ *  - stting
+ *  - array
+ *  - tuple
+ *  - enum
+ *  - void
+ *  - null & undefined
+ *  - any & unknow
+ *  - never
+ * 
+ * 
+ * Reference:
+ *  - https://juejin.cn/post/6844904037922373639
+ */
+
+
+
+
+
+
