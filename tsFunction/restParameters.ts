e54d9310791e687c591195f5f94cd80943227100
linth <zhua0404@gmail.com>
2023-04-05 23:11:59 +0800

    modify rest parameters example.

--- tsFunction/restParameters.ts
+++ tsFunction/restParameters.ts
@@ -5,17 +5,29 @@
  * 
  * Reference:
  *  - https://www.typescripttutorial.net/typescript-tutorial/typescript-rest-parameters/
+ *  - https://www.tutorialsteacher.com/typescript/rest-parameters
  */
 
-function getTotal(...number: number[]): number {
-    let total = 0;
-    number.forEach( (item) => total += item);
-    return total;
-}
 
-console.log(getTotal()); // 0
-console.log(getTotal(10, 20)); // 30
-console.log(getTotal(10, 20, 30)); // 60
+{
+    function getTotal(...number: number[]): number {
+        let total = 0;
+        number.forEach( (item) => total += item);
+        return total;
+    }
+    
+    console.log(getTotal()); // 0
+    console.log(getTotal(10, 20)); // 30
+    console.log(getTotal(10, 20, 30)); // 60
+
+
+
+    function getAListName(...name: string[]) {
+        return name;
+    }
+
+    console.log(getAListName('george', 'may', 'peter')); // [ 'george', 'may', 'peter' ]
+}
 
 
 
