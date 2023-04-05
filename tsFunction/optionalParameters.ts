34138ae34a10bc5a1c33c6da9596a5362504b4b2
linth <zhua0404@gmail.com>
2023-04-05 11:24:53 +0800

    [CF] give a default value to argument.

--- tsFunction/optionalParameters.ts
+++ tsFunction/optionalParameters.ts
@@ -15,7 +15,14 @@ function multiply(a: number, b: number, c?: number): number {
     return a*b;
 }
 
+// after code refactory. (可直接給予預設值)
+function multiply2(a: number, b: number, c: number = 2): number {
+    return a * b * c;
+}
 
-console.log(multiply(1, 2, 3)); // 6
-console.log(multiply(1, 2, 'hi')); // 2
 
+// console.log(multiply(1, 2, 3)); // 6
+// console.log(multiply(1, 2, 'hi')); // 2
+
+console.log(multiply2(1, 2, 3)); // 6
+console.log(multiply2(1, 2)); // 2
