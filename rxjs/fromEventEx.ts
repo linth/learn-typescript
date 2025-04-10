34cd194ff4a8d7e3b584896e1686e9111c17672a
linth <zhua0404@gmail.com>
2025-04-10 18:23:18 +0800

    feat: add fromEvent example.

---
+++ rxjs/fromEventEx.ts
@@ -0,0 +1,12 @@
+/**
+ * fromEvent example.
+ * 
+ */
+
+import { fromEvent } from "rxjs";
+
+
+// const button = document.getElementById('myButton');
+
+// fromEvent(button, 'click')
+// 	.subscribe(() => console.log(`按鈕被點擊了!`))
