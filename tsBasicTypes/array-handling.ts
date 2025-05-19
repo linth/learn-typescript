10e9532612e188908310f97cb966b4c78387e39c
linth <zhua0404@gmail.com>
2025-05-19 17:57:17 +0800

    feat: add array examples: indexOf, filter.

--- tsBasicTypes/array-handling.ts
+++ tsBasicTypes/array-handling.ts
@@ -1,6 +1,8 @@
 /**
  * array handling 
  *  - reduce(): 加總
+ *  - indexOf(): 抓取某array的元素並return index值
+ *  - filter(): 過濾
  * 
  * Reference:
  *  - https://medium.com/%E4%B8%80%E5%80%8B%E5%B0%8F%E5%B0%8F%E5%B7%A5%E7%A8%8B%E5%B8%AB%E7%9A%84%E9%9A%A8%E6%89%8B%E7%AD%86%E8%A8%98/javascript-%E4%BD%BF%E7%94%A8-array-reduce-%E9%80%B2%E8%A1%8C-object-array-%E7%9A%84%E5%8A%A0%E7%B8%BD-ad17e32e3d50
@@ -11,7 +13,7 @@
   const arr = [1, 2, 3, 4, 5, 6];
   let res = arr.reduce((a, b) => a+b);
 
-  console.log(res);
+  console.log(res); // 21
   
   // 加總object
   let arr2 = [
@@ -22,13 +24,37 @@
     { a: 123, b: -1},
   ];
 
-  arr2.reduce((total, currentObj) => {
+  const arr3 = arr2.reduce((total, currentObj) => {
+    console.log('total =', total, 'currentObj =' ,currentObj);
+
     return {
       a: total.a + currentObj.a,
       b: total.b + currentObj.b,
     }
   }, {a: 0, b: 0});
 
-  console.log('arr2', arr2);
+  console.log('arr3', arr3); // arr3 { a: 178, b: 136 }
 }
 
+
+{
+  // 得到某個array的 index
+  const fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
+  let getIndexNumber = fruits.indexOf('Apple');
+  console.log(getIndexNumber); // 2
+
+  // filter
+  const res = fruits.filter(exceptOrange);
+
+  function findOrange(f: string) {
+    if (f === 'Orange') return f;
+  }
+
+  function exceptOrange(f: string) {
+    if (f !== 'Orange') return f;
+    else return;
+  }
+
+  console.log(res);
+  
+}
